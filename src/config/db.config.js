31401c48960f37a8cc51bd3714077fdf0cb40d3c
Saidkamol027 <akhmadjonovsaidkamol027@gmail.com>
2025-03-18 12:31:43 +0500

    Users uchun barcha crud amallar bajarildi

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,15 +1,6 @@
 require('dotenv').config()
 const { Pool } = require('pg')
 
-console.log({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB_NAME,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT,
-});
-
-
 const pool = new Pool({
 	user: process.env.DB_USER,
 	host: process.env.DB_HOST,
--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -3,9 +3,9 @@ const { pool } = require('../config/db.config')
 exports.getAllUsers = async (req, res) => {
 	try {
 		const users = await pool.query('SELECT * FROM users;')
-		res.status(400).json(users.rows)
+		res.status(200).json(users.rows)
 	} catch (error) {
-		throw new Error({ error: 'Server bilan muammo' })
+		res.status(500).json({ error: 'Server bilan muammo' })
 	}
 }
 
@@ -15,12 +15,14 @@ exports.getUsersById = async (req, res) => {
 		const user = await pool.query('SELECT * FROM users WHERE id = $1', [id])
 
 		if (user.rows.length === 0) {
-			res.status(404).json({ error: "Bunda 'id'lik foydalanuvchi yo'q" })
+			return res
+				.status(404)
+				.json({ error: "Bunday 'id'lik foydalanuvchi yo'q" })
 		}
 
-		res.status(200).json(user.rows)
+		res.status(200).json(user.rows[0])
 	} catch (error) {
-		throw new Error({ error: 'Server bilan muammo' })
+		res.status(500).json({ error: 'Server bilan muammo' })
 	}
 }
 
@@ -28,28 +30,29 @@ exports.createUsers = async (req, res) => {
 	try {
 		const { name, email, password } = req.body
 
-		const foundedUser = await pool.query(
-			'SELECT * FROM users WHERE email = $1 AND password = $2 RETURNING *',
+		const existingUser = await pool.query(
+			'SELECT * FROM users WHERE email = $1 AND password = $2',
 			[email, password]
 		)
 
-		if (foundedUser.rowCount) {
-			res
-				.status(409)
-				.json({ message: 'Bunda email va passworlik user allaqachon mavjud' })
+		if (existingUser.rowCount > 0) {
+			return res.status(409).json({
+				message:
+					'Bunday email va passwordga ega foydalanuvchi allaqachon mavjud',
+			})
 		}
 
-		const user = await pool.query(
+		const newUser = await pool.query(
 			'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
 			[name, email, password]
 		)
 
-		res.status(200).json({
-			message: 'User malumotlari muvaffaqaiyatli kiritildi',
-			data: user,
+		res.status(201).json({
+			message: 'User muvaffaqiyatli yaratildi',
+			data: newUser.rows[0],
 		})
 	} catch (error) {
-		throw new Error({ error: 'Server bilan muammo' })
+		res.status(500).json({ error: 'Server bilan muammo' })
 	}
 }
 
@@ -58,26 +61,50 @@ exports.updateUsers = async (req, res) => {
 		const { id } = req.params
 		const { name, email, password } = req.body
 
-		const foundedUser = await pool.query(
-			'SELECT * FROM users WHERE email = $1 AND password = $2 RETURNING *',
-			[email, password]
+		const existingUser = await pool.query(
+			'SELECT * FROM users WHERE email = $1 AND id != $2',
+			[email, id]
 		)
 
-		if (foundedUser.rowCount) {
-			res
+		if (existingUser.rowCount > 0) {
+			return res
 				.status(409)
-				.json({ message: 'Bunda email va passworlik user allaqachon mavjud' })
+				.json({ message: 'Bunday email boshqa foydalanuvchida bor' })
 		}
 
-		const user = await pool.query(
+		const updatedUser = await pool.query(
 			'UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4 RETURNING *',
 			[name, email, password, id]
 		)
 
-		res.status(200).json(user.rows)
+		if (updatedUser.rowCount === 0) {
+			return res
+				.status(404)
+				.json({ message: "Bunday ID'lik foydalanuvchi topilmadi" })
+		}
+
+		res.status(200).json(updatedUser.rows[0])
 	} catch (error) {
-		throw new Error({ error: 'Server bilan muammo' })
+		res.status(500).json({ error: 'Server bilan muammo' })
 	}
 }
 
+exports.deletUser = async (req, res) => {
+	try {
+		const { id } = req.params
+		const deletedUser = await pool.query(
+			'DELETE FROM users WHERE id = $1 RETURNING *',
+			[id]
+		)
 
+		if (deletedUser.rowCount === 0) {
+			return res
+				.status(404)
+				.json({ error: "Bunday 'id'lik foydalanuvchi yo'q" })
+		}
+
+		res.status(200).json({ message: 'User muvaffaqiyatli o‘chirildi' })
+	} catch (error) {
+		res.status(500).json({ error: 'Server bilan muammo' })
+	}
+}
