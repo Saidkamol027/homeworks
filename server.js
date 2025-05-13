32199df053c0896e8e248482cae65898fad8338c
Saidkamol027 <akhmadjonovsaidkamol027@gmail.com>
2025-05-14 02:06:05 +0500

    vazifa

--- server.js
+++ server.js
@@ -1,68 +1,80 @@
-const fs = require('node:fs')
 const http = require('node:http')
+const fs = require('node:fs')
 const path = require('node:path')
-
-const filePath = path.join(__dirname, 'data', 'fruits.json')
+const { isDeepStrictEqual } = require('node:util')
+const { DatabaseSync } = require('node:sqlite')
+const PORT = 3000
+const filePath = path.join(__dirname, 'data', 'user.json')
+const users = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
 
 const server = http.createServer((req, res) => {
 	const method = req.method
 	const url = req.url
 
-	if (method === 'GET' && url === '/') {
-		const mevalar = fs.readFileSync(filePath, 'utf-8')
-		res.writeHead(200, { 'Content-Type': 'application/json' })
-		res.end(mevalar)
-	} else if (method === 'POST' && url === '/mevalar') {
-		let body = ''
-		req.on('data', chunk => {
-			body += chunk
-		})
-		req.on('end', () => {
-			const fruits = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-			const newFruit = JSON.parse(body)
-			newFruit.id = fruits.length + 1
-			fruits.push(newFruit)
-			fs.writeFileSync(filePath, JSON.stringify(fruits, null, 2))
-			res.writeHead(200, { 'Content-Type': 'application/json' })
-			res.end(JSON.stringify({ message: 'Malumot muvaffaqiyatli yozildi' }))
-		})
-	} else if (method === 'DELETE' && url.startsWith('/mevalar/')) {
-		const id = Number(url.split('/')[2])
-		const fruits = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-		const deleteFruit = fruits.filter(meva => meva.id !== id)
-		fs.writeFileSync(filePath, JSON.stringify(deleteFruit, null, 2))
-		res.writeHead(200, { 'Content-Type': 'application/json' })
-		res.end(JSON.stringify({ message: 'Malumotlar muvaffaqiyatli ochirildi' }))
-	} else if (method === 'PATCH' && url.startsWith('/mevalar/')) {
-		const id = Number(url.split('/')[2])
+	if (method === 'GET' && url === '/users') {
+		const users = fs.readFileSync(filePath, 'utf-8')
+
+		res.writeHead(200)
+		res.end(users)
+	}
+	if (method === 'POST' && url === '/user') {
 		let body = ''
 		req.on('data', chunk => {
 			body += chunk
 		})
 		req.on('end', () => {
 			const data = JSON.parse(body)
-			const fruits = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+			const newUser = { id: users.at(-1).id + 1, ...data }
+			users.push(newUser)
 
-			const update = fruits.map(fruit => {
-				if (fruit.id === id) {
-					return { ...fruit, ...data }
-				}
-				return fruit
-			})
+			fs.writeFileSync(filePath, JSON.stringify(users, null, 2))
 
-			fs.writeFileSync(filePath, JSON.stringify(update, null, 2))
-			res.writeHead(200, { 'Content-Type': 'application/json' })
-			res.end(
-				JSON.stringify({ message: 'Malumotlar muvaffaqiyatli yangilandi!' })
-			)
+			res.writeHead(200)
+			res.end(JSON.stringify(newUser))
 		})
-	} else {
-		res.writeHead(404, { 'Content-Type': 'application/json' })
-		res.end(JSON.stringify({ message: 'Not Found' }))
+	}
+	if (method === 'PATCH' && url.startsWith('/user/')) {
+		const id = url.split('/')[2]
+		const index = users.findIndex(user => user.id == id)
+
+		if (index === -1) {
+			return res.end(JSON.stringify({ message: 'User not found' }))
+		}
+
+		let body = ''
+		req.on('data', chunk => {
+			body += chunk
+		})
+		req.on('end', () => {
+			const data = JSON.parse(body)
+			users[index].name = data.name
+			users[index].age = data.age
+
+			fs.writeFileSync(filePath, JSON.stringify(users, null, 2))
+
+			res.writeHead(200)
+			res.end(JSON.stringify(users))
+		})
+	}
+
+	if (method === 'DELETE' && url.startsWith('/user/')) {
+		const id = url.split('/')[2]
+		const index = users.findIndex(user => user.id == id)
+
+		if (index === -1) {
+			res.writeHead(404)
+			return res.end(JSON.stringify({ message: 'User not found' }))
+		}
+
+		users.splice(index, 1)
+
+		fs.writeFileSync(filePath, JSON.stringify(users, null, 2))
+
+		res.writeHead(200)
+		res.end(JSON.stringify({ message: 'User deleted successfully' }))
 	}
 })
 
-const PORT = 3000
 server.listen(PORT, () => {
 	console.log(`Server ${PORT} da ishlamoqda`)
 })
