69abd9e35562cb7dd63d78e02e2c1efb1484fba5
Saidkamol027 <akhmadjonovsaidkamol027@gmail.com>
2025-05-17 23:53:28 +0500

    vazifa

--- src/model/product.model.js
+++ src/model/product.model.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 
 const productSchema = new mongoose.Schema(
 	{
-		name: { type: String },
-		color: { type: String },
+		name: { type: String, required: true },
+		price: { type: String, required: true },
 	},
 	{ versionKey: false, collection: 'product' }
 )
