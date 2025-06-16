8ef81870049b904b1faa7d26e575d34fbdba921c
Saidkamol027 <akhmadjonovsaidkamol027@gmail.com>
2025-06-16 16:11:12 +0500

    vazifa

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -1,14 +1,47 @@
 import { Router } from 'express'
 import { CustomerController } from '../controllers/customer.controller.js'
+import { ValidateMiddleware } from '../middlewares/validations.middleware.js'
+import {
+	forgotPasswordValidation,
+	forgotVerifyOtpValidation,
+	loginCustomerValidation,
+	registerCustomerValidation,
+	registerVerifyOtpValidation,
+	resetPasswordValidation,
+} from '../validations/customer.validation.js'
 
 const customerRoute = Router()
 const controller = new CustomerController()
 
-customerRoute.post('/register', controller.register)
-customerRoute.post('/register-verify-otp', controller.registerVerifyOtp)
-customerRoute.post('/forgot-password', controller.forgotPassword)
-customerRoute.post('/forgot-verify-otp', controller.forgotVerifyOtp)
-customerRoute.post('/reset-password', controller.resetPassword)
-customerRoute.post('/login', controller.login)
+customerRoute.post(
+	'/register',
+	ValidateMiddleware(registerCustomerValidation),
+	controller.register
+)
+customerRoute.post(
+	'/register-verify-otp',
+	ValidateMiddleware(registerVerifyOtpValidation),
+	controller.registerVerifyOtp
+)
+customerRoute.post(
+	'/forgot-password',
+	ValidateMiddleware(forgotPasswordValidation),
+	controller.forgotPassword
+)
+customerRoute.post(
+	'/forgot-verify-otp',
+	ValidateMiddleware(forgotVerifyOtpValidation),
+	controller.forgotVerifyOtp
+)
+customerRoute.post(
+	'/reset-password',
+	ValidateMiddleware(resetPasswordValidation),
+	controller.resetPassword
+)
+customerRoute.post(
+	'/login',
+	ValidateMiddleware(loginCustomerValidation),
+	controller.login
+)
 
 export { customerRoute }
---
+++ src/validations/customer.validation.js
@@ -0,0 +1,31 @@
+import Joi from 'joi'
+
+export const registerCustomerValidation = Joi.object({
+	name: Joi.string().min(3).max(50).required(),
+	email: Joi.string().email().required(),
+	password: Joi.string().min(6).max(100).required(),
+})
+
+export const registerVerifyOtpValidation = Joi.object({
+	email: Joi.string().email().required(),
+	otp: Joi.string().length(6).required(),
+})
+
+export const forgotPasswordValidation = Joi.object({
+	email: Joi.string().email().required(),
+})
+
+export const forgotVerifyOtpValidation = Joi.object({
+	email: Joi.string().email().required(),
+	otp: Joi.string().length(6).required(),
+})
+
+export const resetPasswordValidation = Joi.object({
+	email: Joi.string().email().required(),
+	newPassword: Joi.string().min(6).max(100).required(),
+})
+
+export const loginCustomerValidation = Joi.object({
+	email: Joi.string().email().required(),
+	password: Joi.string().required(),
+})
